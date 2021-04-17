5546f59accb71f08b82d5e13574f4b1ab5086211
Jerome Lecomte <jerome@herebefrogs.com>
2021-04-17 18:20:11 -0400

    scale text spacing as well
    spacing was still 1px no matter how many times the text was scaled up

--- src/js/text.js
+++ src/js/text.js
@@ -34,7 +34,7 @@ export const initCharset = async (canvasContext) => {
  */
 export function renderText(msg, x, y, align = ALIGN_LEFT, scale = 1) {
   const SCALED_SIZE = scale * CHARSET_SIZE;
-  const MSG_WIDTH = msg.length * (SCALED_SIZE + 1) - 1;
+  const MSG_WIDTH = msg.length * SCALED_SIZE + (msg.length - 1) * scale;
   const ALIGN_OFFSET = align === ALIGN_RIGHT ? MSG_WIDTH :
                        align === ALIGN_CENTER ? MSG_WIDTH / 2 :
                        0;
@@ -42,8 +42,8 @@ export function renderText(msg, x, y, align = ALIGN_LEFT, scale = 1) {
     ctx.drawImage(
       charset,
       // TODO could memoize the characters index or hardcode a lookup table
-      ALPHABET.indexOf(c)*CHARSET_SIZE, 0, CHARSET_SIZE, CHARSET_SIZE,
-      x + i*(SCALED_SIZE + 1) - ALIGN_OFFSET, y, SCALED_SIZE, SCALED_SIZE
+      ALPHABET.indexOf(c) * CHARSET_SIZE, 0, CHARSET_SIZE, CHARSET_SIZE,
+      x + i * scale * (CHARSET_SIZE + 1) - ALIGN_OFFSET, y, SCALED_SIZE, SCALED_SIZE
     );
   });
 };
